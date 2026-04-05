c42c4f5ccdeb758b674b850300aac347bac912a3
adityakumarpattnaik143 <97521512+adityapattnaiknist@users.noreply.github.com>
2026-04-05 16:06:11 +0530

    checked

--- daily_codes/calculator.py
+++ daily_codes/calculator.py
@@ -5,3 +5,7 @@ def add(a, b):
 def subtract(a, b):
     """Returns the difference when b is subtracted from a."""
     return a - b
+
+def multiple(a, b):
+    """returns the multiplication when b is multiplied from a."""
+    return a * b
--- daily_codes/test_calculator.py
+++ daily_codes/test_calculator.py
@@ -1,4 +1,4 @@
-from daily_codes.calculator import add, subtract
+from daily_codes.calculator import add, subtract, multiple
 
 def test_add():
     # This checks if the exact output of add(2, 3) is 5
@@ -8,4 +8,9 @@ def test_add():
 def test_subtract():
     # This checks if the exact output of subtract(5, 3) is 2
     assert subtract(5, 3) == 2
-    assert subtract(10, 20) == -10
+    assert subtract(10, 20) == -150
+    
+def test_multiple():
+    assert multiple(2, 3) == 6
+    assert multiple(4, 3) ==12
+    
